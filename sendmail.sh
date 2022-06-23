05104aecb9bd2ef7c1799014f06cfe8ee41a45e5
jeankon <jeankon@gmail.com>
2022-06-23 13:04:03 +0800

    邮件发送脚本

--- sendmail.sh
+++ sendmail.sh
@@ -4,7 +4,7 @@ login=0
 for i in `seq 3`; do
   code=$(awk 'BEGIN{srand();printf("%06d",1000000*rand())}')
   /sendmail/bin/sendmail /sendmail/bin/sendmail.json "$code"
-  if ! read -t 60 -s -p 'code: ' inputCode ;then
+  if ! read -t 60 -s -p '验证码: ' inputCode ;then
     exit 0
   fi
   echo
